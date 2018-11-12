2d9c4d16625b54fdfe582834734ca9c8b3d6c301
Andrew Ghostuhin <me@torinasakura.name>
2018-11-12 05:16:07 +0300

    feat($swt): refactor delivering footer content and background changes
    $swt refactor:
    - cleanup App container
    - move base markup to Main page component
    - move InjectIntl to Main page component
    
    Support:
    - need update from UI
    
    Signed-off-by: Andrew Ghostuhin <me@torinasakura.name>

--- packages/swt/src/pages/main/components/desktop/Main.js
+++ packages/swt/src/pages/main/components/desktop/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { injectIntl } from 'react-intl'
 import { Column, Layout, Row } from 'flex-layouts'
 import {
   Balances,
@@ -10,8 +11,12 @@ import {
   TradingHistory,
   SolidityDetail,
 } from '@er/ui/src/components'
+import { Text } from '@er/ui/src/text'
+import { Block } from '@er/ui/src/content'
+import messages from '../../../../messages'
 
 const Main = ({
+  intl,
   match,
   toggle,
   tab,
@@ -20,59 +25,82 @@ const Main = ({
   onChangeTab,
   onChangeAmount,
 }) => (
-  <Column align='center'>
-    <Layout>
-      <Row>
-        <Layout>
-          <Balances />
-        </Layout>
-        <Layout>
-          <Chart />
-        </Layout>
-      </Row>
-    </Layout>
-    <Layout>
-      <Row>
-        <Layout>
-          <ChoosePair
-            tab={tab}
-            onChangeTab={onChangeTab}
-            match={match}
-          />
-        </Layout>
-        <Layout>
-          <Pair
-            amount={amount}
-            toggle={toggle}
-            onChangeToggle={onChangeToggle}
-            onChangeAmount={onChangeAmount}
-          />
-        </Layout>
-        <Layout>
-          <BuyOrders
-            activeTab={toggle}
-          />
-        </Layout>
-      </Row>
-    </Layout>
-    <Layout>
-      <Row>
-        <Layout>
-          <Column>
-            <Layout>
-              <ActiveOrders />
-            </Layout>
-            <Layout>
-              <SolidityDetail />
-            </Layout>
-          </Column>
-        </Layout>
-        <Layout>
-          <TradingHistory />
-        </Layout>
-      </Row>
-    </Layout>
-  </Column>
+  <Block>
+    <Column align='center'>
+      <Layout basis='32px' />
+      <Layout>
+        <Row>
+          <Layout>
+            <Balances />
+          </Layout>
+          <Layout>
+            <Chart />
+          </Layout>
+        </Row>
+      </Layout>
+      <Layout>
+        <Row>
+          <Layout>
+            <ChoosePair
+              tab={tab}
+              onChangeTab={onChangeTab}
+              match={match}
+            />
+          </Layout>
+          <Layout>
+            <Pair
+              amount={amount}
+              toggle={toggle}
+              onChangeToggle={onChangeToggle}
+              onChangeAmount={onChangeAmount}
+            />
+          </Layout>
+          <Layout>
+            <BuyOrders
+              activeTab={toggle}
+            />
+          </Layout>
+        </Row>
+      </Layout>
+      <Layout>
+        <Row>
+          <Layout>
+            <Column>
+              <Layout>
+                <ActiveOrders />
+              </Layout>
+              <Layout>
+                <SolidityDetail />
+              </Layout>
+            </Column>
+          </Layout>
+          <Layout>
+            <TradingHistory />
+          </Layout>
+        </Row>
+      </Layout>
+      <Layout basis='75px' grow={1} />
+      <Layout justify='center'>
+        <Text
+          size='xsmall'
+          weight='medium'
+          color='gray900'
+        >
+          {intl.formatMessage(messages.copyright)}
+        </Text>
+      </Layout>
+      <Layout justify='center'>
+        <Text
+          size='xsmall'
+          weight='medium'
+          color='gray900'
+        >
+          {intl.formatMessage(messages.address)}
+        </Text>
+      </Layout>
+      <Layout basis='40px' />
+    </Column>
+  </Block>
 )
 
-export default Main
+export default injectIntl(Main)
